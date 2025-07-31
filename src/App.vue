552cc8e498eb235a0af2081d100a3c4f94823d75
vito14565 <vito14565@gmail.com>
2025-07-31 22:44:55 +1000

    Initial commit for FIT5032 Task 1

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MyName from './components/MyName.vue'
+</script>
+
+<template>
+  <h1>Hello from FIT5032</h1>
+  <MyName />
+</template>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
---
+++ src/components/MyName.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>This is my custom component</h2>
+    <p>My name is Wei-Hsiang Liu.</p>
+    <p>I’m learning Vue in FIT5032.</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  color: #2c3e50;
+}
+</style>
