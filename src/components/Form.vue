0d22af8f612012168ff51462a436dbeafae38c7f
vito14565 <vito14565@gmail.com>
2025-08-11 23:42:48 +1000

    Week 3 task 1.2 feat: add Form.vue component with initial template

---
+++ src/components/Form.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container py-4">
+    <h1>User Information Form / Credentials</h1>
+  </div>
+</template>
+
+<script setup>
+// The form logic will be added here later
+</script>
+
+<style scoped>
+/* Custom styles will be added here later */
+</style> 
