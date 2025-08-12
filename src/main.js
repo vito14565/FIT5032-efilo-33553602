b639ce41cf493b82326cde2135855efc6b527005
vito14565 <vito14565@gmail.com>
2025-08-12 22:32:38 +1000

    import Bootstrap CSS

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
 
 // import Bootstrap CSS and JS
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
+
+// import './style.css'
 
 createApp(App).mount('#app')
