a813f61e4e48426f8a57c3cd86458108dc3bb97b
vito14565 <vito14565@gmail.com>
2025-08-12 22:34:20 +1000

    feat(ui): restyle form with Bootstrap classes and add v-model handlers

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,33 +1,103 @@
 <template>
-  <div class="form">
-    <h1>User Information Form / Credentials</h1>
-    <form>
-      <label for="username">Username:</label><br>
-      <input type="text" id="username" name="username"><br>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center">User Information Form</h1>
 
-      <label for="password">Password:</label><br>
-      <input type="password" id="password" name="password"><br>
+        <form @submit.prevent="submitForm">
+          <!-- username + password -->
+          <div class="row mb-3">
+            <div class="col-md-6">
+              <label for="username" class="form-label">Username</label>
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                v-model="formData.username"
+              />
+            </div>
+            <div class="col-md-6">
+              <label for="password" class="form-label">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                v-model="formData.password"
+              />
+            </div>
+          </div>
 
-      <label for="isAustralian">Australian Resident?</label><br>
-      <input type="checkbox" id="isAustralian" name="isAustralian"><br>
+          <!-- checkbox + gender -->
+          <div class="row mb-3">
+            <div class="col-md-6">
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  id="isAustralian"
+                  v-model="formData.isAustralian"
+                />
+                <label class="form-check-label" for="isAustralian">
+                  Australian Resident?
+                </label>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <label for="gender" class="form-label">Gender</label>
+              <select id="gender" class="form-select" v-model="formData.gender">
+                <option value="female">Female</option>
+                <option value="male">Male</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+          </div>
 
-      <label for="reason">Reason For Joining:</label><br>
-      <textarea id="reason" name="reason" rows="3"></textarea><br>
+          <!-- reason -->
+          <div class="mb-3">
+            <label for="reason" class="form-label">Reason for joining</label>
+            <textarea
+              class="form-control"
+              id="reason"
+              rows="3"
+              v-model="formData.reason"
+            ></textarea>
+          </div>
 
-      <label for="gender">Gender</label><br>
-      <select id="gender">
-        <option value="female">Female</option>
-        <option value="male">Male</option>
-        <option value="other">Other</option>
-      </select>
-    </form>
+          <!-- buttons -->
+          <div class="text-center">
+            <button type="submit" class="btn btn-primary me-2">Submit</button>
+            <button type="button" class="btn btn-secondary" @click="clearForm">
+              Clear
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-// Logic will be added later
-</script>
+import { reactive } from 'vue'
 
-<style scoped>
-/* Bootstrap / CSS styling will be added later */
-</style>
+const formData = reactive({
+  username: '',
+  password: '',
+  isAustralian: false,
+  gender: 'female',
+  reason: ''
+})
+
+function submitForm() {
+  
+  console.log('Submitted:', JSON.parse(JSON.stringify(formData)))
+  alert('Form submitted!')
+}
+
+function clearForm() {
+  formData.username = ''
+  formData.password = ''
+  formData.isAustralian = false
+  formData.gender = 'female'
+  formData.reason = ''
+}
+</script>
