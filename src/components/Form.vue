74fd3fdd63daeec2fe3cc1d4537a39462a2ca4e8
vito14565 <vito14565@gmail.com>
2025-08-12 23:02:04 +1000

    feat: update User Information Form to be responsive with Bootstrap breakpoints (Activity 7)

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,24 +7,24 @@
         <!-- Form -->
         <form @submit.prevent="submitForm">
           <div class="row mb-3">
-            <div class="col-md-6">
+            <div class="col-12 col-md-6">
               <label for="username" class="form-label">Username</label>
               <input type="text" class="form-control" id="username" v-model="formData.username" />
             </div>
-            <div class="col-md-6">
+            <div class="col-12 col-md-6">
               <label for="password" class="form-label">Password</label>
               <input type="password" class="form-control" id="password" v-model="formData.password" />
             </div>
           </div>
 
           <div class="row mb-3">
-            <div class="col-md-6">
+            <div class="col-12 col-md-6">
               <div class="form-check">
                 <input class="form-check-input" type="checkbox" id="isAustralian" v-model="formData.isAustralian" />
                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
               </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-12 col-md-6">
               <label for="gender" class="form-label">Gender</label>
               <select id="gender" class="form-select" v-model="formData.gender">
                 <option value="female">Female</option>
